d64f58a45d0987d52fb5fc533b2e074aa6c0a7e3
harshita <hrs.dadhich@gmail.com>
2024-05-18 23:37:32 +0530

    index url added

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
-from .views import movieListView, platformListView
+from .views import (
+    movieListView,
+    platformListView,
+    movieListIndexView,
+    platformListIndexView,
+)
 
-urlpatterns = [path("list/", movieListView), path("platform/", platformListView)]
+urlpatterns = [
+    path("list/", movieListView),
+    path("list/<int:pk>", movieListIndexView),
+    path("platform/", platformListView),
+    path("platform/<int:pk>", platformListIndexView),
+]
--- core/home/views.py
+++ core/home/views.py
@@ -7,14 +7,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def movieListView(request):
-    print(request)
     if request.method == "GET":
         movies = MovieList.objects.all()
         serializer = MovieListSerializer(movies, many=True)
         return JsonResponse(serializer.data, safe=False)
     elif request.method == "POST":
         data = JSONParser().parse(request)
-        print("data:- ", data)
         postSerializer = MovieListSerializer(data=data)
         if postSerializer.is_valid():
             postSerializer.save()
@@ -22,7 +20,28 @@ def movieListView(request):
         return JsonResponse(postSerializer.errors, status=400)
 
 
+def movieListIndexView(request, pk):
+    movies = MovieList.objects.get(pk=pk)
+    serializer = MovieListSerializer(movies)
+    return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
 def platformListView(request):
-    platformModal = PlatformList.objects.all()
-    serializer = PlatformListSerializer(platformModal, many=True)
+    if request.method == "GET":
+        platformModal = PlatformList.objects.all()
+        serializer = PlatformListSerializer(platformModal, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = PlatformListSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.data, status=400)
+
+
+def platformListIndexView(request, pk):
+    platformModal = PlatformList.objects.get(pk=pk)
+    serializer = PlatformListSerializer(platformModal)
     return JsonResponse(serializer.data, safe=False)
