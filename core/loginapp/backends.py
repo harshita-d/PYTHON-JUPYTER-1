c6097d48ac3793fd4f18ad7568af2c37d59fbac5
harshita <hrs.dadhich@gmail.com>
2024-06-21 16:08:20 +0530

    admin issue fix

--- core/loginapp/backends.py
+++ core/loginapp/backends.py
@@ -18,3 +18,10 @@ class EmailAuthBackend(BaseBackend):
             return user
         except User.DoesNotExist:
             return None
+
+    def get_user(self, user_id):
+        User = get_user_model()
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
